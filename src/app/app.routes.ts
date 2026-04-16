4d021e0d00fec21232ff28e52382fc7c8bb633e2
GeovanaLima06 <geovanalimasoares624@gmail.com>
2026-04-16 16:52:06 -0500

    subidinha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Tendenciasmasc } from './tendenciasmasc/tendenciasmasc';
+import { Tendenciasfem } from './tendenciasfem/tendenciasfem';
+import { SectionCenter } from './section-center/section-center';
+import { SecaoCalcados } from './secao-calcados/secao-calcados';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SectionCenter },
+  { path: 'calcados', component: SecaoCalcados },
+  { path: 'tendencias-masculino', component: Tendenciasmasc },
+  { path: 'tendencias-feminino', component: Tendenciasfem },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from './menu/menu';
+import { SectionCenter } from './section-center/section-center';
+import { SecaoCalcados } from './secao-calcados/secao-calcados';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Menu, SectionCenter, SecaoCalcados],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('StylepShere');
+  protected readonly title = signal('StylepSphere');
 }
